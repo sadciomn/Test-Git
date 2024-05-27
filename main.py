338c7614652b5922319936bd4eb2610641d6a06a
sadciomn <sadciomngit@gmail.com>
2024-05-27 17:51:53 +0300

    Replaced test file on main file Function that show the index number of letters in sentence

---
+++ main.py
@@ -0,0 +1,10 @@
+def main(text):
+    output = ""
+    for x in text:
+        if 1 <= (ord(x.lower()) - 96) <= 27:
+            output += str(ord(x.lower()) - 96)
+            output += " "
+    return output.rstrip(" ") # Deletes space in the end of the result
+
+if __name__ == '__main__':
+    main(input('Input your sentence: '))
